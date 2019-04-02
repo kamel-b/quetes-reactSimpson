305fba55e51151e53d7170054c235dbac0433ee8
Kamel Belkheiri <kamel.belkheiri75@gmail.com>
2019-04-02 11:52:33 +0200

    React 3 button work not working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Quote from './components/Quote';
+import Quotes from './components/Quotes'
+import Lamp from './components/Lamp';
 
+const user = {
+  name : "kamel",
+  city : "Paris",
+  age : "28 ans"
+}
 class App extends Component {
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header {...user}/>
+        <Quote/>
+        <Quotes/>
+        <Lamp /> 
       </div>
     );
   }
 }
 
-export default App;
+export default App
